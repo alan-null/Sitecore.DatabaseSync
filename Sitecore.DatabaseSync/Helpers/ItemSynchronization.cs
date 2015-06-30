26f1d49ffd9530364dcfd9df2ee5008421b55c13
Alan NULL <alan-null@outlook.com>
2015-06-30 23:46:00 +0200

    BasicProvider rewritten. From now it uses SyncItem from Sitecore.

---
+++ Sitecore.DatabaseSync/Helpers/ItemSynchronization.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using Sitecore.Data.Serialization.ObjectModel;
+
+namespace Sitecore.DatabaseSync.Helpers
+{
+    public class ItemSynchronization
+    {
+        public static SyncItem GetSyncItem(string path)
+        {
+            using (TextReader reader = new StreamReader(File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read)))
+            {
+                return SyncItem.ReadItem(new Tokenizer(reader));
+            }
+        }
+    }
+}
--- Sitecore.DatabaseSync/Providers/BasicProvider.cs
+++ Sitecore.DatabaseSync/Providers/BasicProvider.cs
@@ -1,41 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Sitecore.Configuration;
+using Sitecore.DatabaseSync.Helpers;
 using Sitecore.DatabaseSync.Models;
 
 namespace Sitecore.DatabaseSync.Providers
 {
-    class BasicProvider : IDatabaseSyncChangesProvider
+    public class BasicProvider : IDatabaseSyncChangesProvider
     {
         public List<ItemChange> GetItems()
         {
-            var serializationFiles = Directory.GetFiles(Configuration.Settings.DataFolder, "*.item", SearchOption.AllDirectories);
-            var rawFields = serializationFiles.Select(ReadItem);
-            return rawFields.Select(id => new ItemChange
+            var serializationFiles = Directory.GetFiles(Settings.DataFolder, "*.item", SearchOption.AllDirectories);
+            var syncItems = serializationFiles.Select(ItemSynchronization.GetSyncItem);
+            return syncItems.Select(id => new ItemChange
             {
-                Database = id["database"].TrimStart(' '),
-                SitecorePath = id["path"].TrimStart(' ')
+                Database = id.DatabaseName,
+                SitecorePath = id.ItemPath
             }).ToList();
-        }
-
-        private Dictionary<string, string> ReadItem(string path)
-        {
-            List<string> lines = GetRawContent(path);
-            List<string> range = GetSection(@"----item----", lines);
-            return range.Select(x => x.Split(':')).ToList().ToDictionary(s => s[0], s => s[1]);
-        }
-
-        private static List<string> GetSection(string section, List<string> lines)
-        {
-            var startIndex = lines.IndexOf(section);
-            int finishIndex = lines.IndexOf(String.Empty, startIndex);
-            return lines.GetRange(startIndex + 1, finishIndex - 1);
-        }
-
-        private static List<string> GetRawContent(string path)
-        {
-            return File.ReadAllLines(path).ToList();
         }
     }
 }
